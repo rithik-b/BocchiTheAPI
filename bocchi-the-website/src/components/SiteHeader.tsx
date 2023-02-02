6a643840355de9eb649007388410f5c216b26013
PixelBoom <rithikbhatia12@gmail.com>
2023-02-02 12:18:24 +0400

    Improved responsive layout

--- bocchi-the-website/src/components/SiteHeader.tsx
+++ bocchi-the-website/src/components/SiteHeader.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "@components/ThemeToggle"
 function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 grid w-full grid-cols-3 border-b border-b-slate-200 bg-white p-4 dark:border-b-slate-900 dark:bg-slate-800">
-      <h1 className="col-start-2 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight sm:col-start-1 sm:text-left md:col-start-2 md:text-center">
         Boccher
       </h1>
       <div className="col-start-3 flex items-center justify-end">
